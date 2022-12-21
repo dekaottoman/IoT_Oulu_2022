8a1a8ed4a9a6231a1b654435bcb9d05c0fda1561
Teoman Berkay Ayaz <78770372+dekaottoman@users.noreply.github.com>
2022-12-21 15:13:27 +0200

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -26,7 +26,7 @@ def write_file(file:str,file_path:str):
 
 def create_node(data,folder_len:int,folder_path:str):
     if folder_len > 0:
-        previous = read_file( folder_path + "\\" + ("node_" + str(folder_len - 2) + ".txt")) #NEEDS UPDATE
+        previous = read_file( folder_path + "/" + ("node_" + str(folder_len - 2) + ".txt")) #NEEDS UPDATE
         prev_hash = hashlib.sha256(previous.encode("utf-8")).hexdigest()
     else:
         prev_hash = "NULL"
@@ -50,21 +50,22 @@ def add_node(data,folder_path:str):
     stop, i = False, 0
     while not stop:
         check_path = "node_"+ str(i) +".txt"
-        check = pathlib.Path( folder_path + "\\" + check_path)
+        check = pathlib.Path( folder_path + "/" + check_path)
         if not check.exists():
-            write_file(node, folder_path + "\\" + check_path)
+            write_file(node, folder_path + "/" + check_path)
             stop = True
         i += 1
 
     folder = read_folder(folder_path)
     full_chain = ""
     for f in folder:
-        f_path =  folder_path + "\\" + f
-        current = read_file(f_path)
-        full_chain += current
+        if f != "chain_hash.txt":
+            f_path =  folder_path + "/" + f
+            current = read_file(f_path)
+            full_chain += current
     
     chain_hash = hashlib.sha256(full_chain.encode("utf-8")).hexdigest()
-    write_file(chain_hash, folder_path + "\\" + "chain_hash.txt")
+    write_file(chain_hash, folder_path + "/" + "chain_hash.txt")
 
 if __name__ == "__main__":
     folder_path = input("Enter the path to the folder containing the blockchain\t>>")
@@ -73,7 +74,6 @@ if __name__ == "__main__":
     
     data = input("Enter node data >>")
     """node = create_node(data,len(folder), folder_path)
-
     stop, i = False, 0
     while not stop:
         check_path = "node_"+ str(i) +".txt"
@@ -82,7 +82,6 @@ if __name__ == "__main__":
             write_file(node, folder_path + "\\" + check_path)
             stop = True
         i += 1
-
     folder = read_folder(folder_path)
     display_node(node)
     
